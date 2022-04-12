d404193a9811b0c379a92b2b5034f5f0330056d2
ilerik <user@LAPTOP-HJKKDMLP.localdomain>
2022-04-12 15:29:46 +0300

    test suite setup for contracts

---
+++ contracts/pow/src/lib.rs
@@ -0,0 +1,52 @@
+use near_sdk::json_types::Base64VecU8;
+use near_sdk::serde_json::json;
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{env, log, near_bindgen, PanicOnDefault, AccountId, BorshStorageKey};
+use near_sdk::collections::{ LookupMap, LazyOption, Vector, UnorderedSet };
+
+mod constants;
+pub mod views;
+
+#[derive(Clone)]
+#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct EvidenceData {
+    media_hash: String,
+    metadata: String,
+}
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Contract {
+    owner_id: AccountId, // Owner ID
+    evidence: Vector<EvidenceData>
+}
+
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    Evidence,
+}
+
+#[near_bindgen]
+impl Contract {
+    /// Initializes the contract owned by `owner_id`
+    #[init]
+    pub fn new() -> Self {    
+        assert!(!env::state_exists(), "Already initialized");
+        let owner_id = env::current_account_id(); // Who deployed owns
+ 
+        Self {
+            owner_id: owner_id,
+            evidence: Vector::new(StorageKey::Evidence),            
+        }
+    }
+    
+
+    /// Upload evidence fingerprint to blochchain
+    #[payable]
+    pub fn upload_evidence(&mut self, evidence: EvidenceData) {    
+        let timestamp: u64 = env::block_timestamp();
+        self.evidence.push(&evidence);
+    }
+}
