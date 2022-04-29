cf815d28ddf5a003a5297b2b3736c535d81e38eb
SergantChe <sergei.alex.kozlov@gmail.com>
2022-04-29 05:08:49 +0300

    Add view method and write some tests

--- contracts/pow/src/views.rs
+++ contracts/pow/src/views.rs
@@ -7,9 +7,14 @@ impl Contract {
         env!("CARGO_PKG_VERSION").to_string()
     }
 
-    /// Get all user actions for current event (supports pagination)
+    /// Get amount of uploaded evidences
+    pub fn get_evidences_amount(&self) -> u64 {
+        self.evidence.len()
+    }
+
+    /// Get all user evidences (supports pagination)
     /// - `from_index` is the index to start from.
-    /// - `limit` is the maximum number of elements to return.
+    /// - `limit` is the maximum number of elements (evidences) to return.
     pub fn get_evidences(&self, from_index: u64, limit: u64) -> Vec<EvidenceData> {
         (from_index..std::cmp::min(from_index + limit, self.evidence.len()))
             .map(|index| self.evidence.get(index).unwrap())
