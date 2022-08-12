f7745ee3905a400118670503c45c5654fb1f859e
SergantChe <sergei.alex.kozlov@gmail.com>
2022-08-12 13:56:06 +0300

    replace images for test events

--- contracts/events/tests/start_events.js
+++ contracts/events/tests/start_events.js
@@ -24,14 +24,14 @@ const startEvent1 = `near call ${contractName} start_event '{"event_data": {
     "qr_prefix_len": ${"hello".length},
     "reward_description": "Welcome to the test!",
     "reward_title": "vSelf: Tester Badge",
-    "reward_uri": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Felis_silvestris_silvestris.jpg/1200px-Felis_silvestris_silvestris.jpg"
+    "reward_uri": "https://us.123rf.com/450wm/oksanastepova/oksanastepova1805/oksanastepova180500047/102167642-hello-unique-hand-drawn-nursery-poster-with-lettering-in-scandinavian-style-vector-illustration-.jpg?ver=6"
   },
   {
     "qr_prefix_enc": "${hash('goodbye')}",
     "qr_prefix_len": ${"goodbye".length},
     "reward_description": "Welcome to the test!",
     "reward_title": "vSelf: Tester Badge",
-    "reward_uri": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Zunge_raus.JPG/1200px-Zunge_raus.JPG"
+    "reward_uri": "https://www.wallquotes.com/sites/default/files/entr0054_01.jpg"
   }],
 "start_time": ${start_time}}}' --accountId ${masterAccount}`;
 
@@ -45,21 +45,21 @@ const startEvent2 = `near call ${contractName} start_event '{"event_data": {
     "qr_prefix_len": ${"cat".length},
     "reward_description": "You discovered a cat!",
     "reward_title": "vSelf: Tester Badge",
-    "reward_uri": "https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg"
+    "reward_uri": "https://i.pinimg.com/736x/98/16/ee/9816ee545b0f22fed08d3e120fdd48d7--jungle-cat-a-tiger.jpg"
   },
   {
     "qr_prefix_enc": "${hash('dog')}",
     "qr_prefix_len": ${"dog".length},
     "reward_description": "You discovered a dog!",
     "reward_title": "vSelf: Tester Badge",
-    "reward_uri": "https://th-thumbnailer.cdn-si-edu.com/SdKYWifCKfE2g8O-po_SO99hQ-Y=/1000x750/filters:no_upscale():focal(3126x2084:3127x2085)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer_public/ec/e6/ece69181-708a-496e-b2b7-eaf7078b99e0/gettyimages-1310156391.jpg"
+    "reward_uri": "https://breed-assets.wisdompanel.com/dog/street-dog-india/Indian_Street_Dog_Color.png"
   },
   {
     "qr_prefix_enc": "${hash('possum')}",
     "qr_prefix_len": ${"possum".length},
     "reward_description": "You discovered a possum!",
     "reward_title": "vSelf: Tester Badge",
-    "reward_uri": "https://www.rd.com/wp-content/uploads/2021/04/GettyImages-139677758-scaled.jpg"
+    "reward_uri": "https://i.pinimg.com/550x/43/e2/26/43e226270cbc24c2fd7efeff710da7d1.jpg"
   }],
 "start_time": ${start_time}}}' --accountId ${masterAccount}`;
 
