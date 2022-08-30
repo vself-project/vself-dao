20a33c2793c1d22bda109a17ed5e7f0318309bcf
ilerik <user@LAPTOP-HJKKDMLP.localdomain>
2022-08-30 15:48:19 +0300

    bug fixes for v3

--- contracts/events/src/lib.rs
+++ contracts/events/src/lib.rs
@@ -1,17 +1,17 @@
 use near_contract_standards::non_fungible_token::metadata::{
-    NFTContractMetadata, NFT_METADATA_SPEC, TokenMetadata
+    NFTContractMetadata, TokenMetadata, NFT_METADATA_SPEC,
 };
-use near_contract_standards::non_fungible_token::{NonFungibleToken};
-use near_sdk::json_types::Base64VecU8;
+use near_contract_standards::non_fungible_token::NonFungibleToken;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet, Vector};
+use near_sdk::json_types::Base64VecU8;
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{env, log, near_bindgen, PanicOnDefault, AccountId, BorshStorageKey};
-use near_sdk::collections::{ LookupMap, LazyOption, Vector, UnorderedSet, UnorderedMap };
+use near_sdk::{env, log, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault};
 use std::collections::HashSet;
 
 mod constants;
-pub mod views;
 pub mod nft;
+pub mod views;
 
 /// It's used to compute event id (start_event function)
 fn read_be_u32(input: &mut &[u8]) -> u32 {
@@ -20,22 +20,18 @@ fn read_be_u32(input: &mut &[u8]) -> u32 {
     u32::from_be_bytes(int_bytes.try_into().unwrap())
 }
 
-#[derive(Clone)]
-#[derive(Debug)]
-#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+#[derive(Clone, Debug, BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct QuestData {
     pub qr_prefix_enc: String,
-    pub qr_prefix_len: usize,    
+    pub qr_prefix_len: usize,
     pub reward_title: String,
     pub reward_description: String,
     pub reward_uri: String,
 }
 
 /// Current event data
-#[derive(Clone)]
-#[derive(Debug)]
-#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+#[derive(Clone, Debug, BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct EventData {
     event_name: String,
@@ -46,11 +42,9 @@ pub struct EventData {
 }
 
 /// Current event data
-#[derive(Clone)]
-#[derive(Debug)]
-#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+#[derive(Clone, Debug, BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
-pub struct EventStats {               
+pub struct EventStats {
     participants: HashSet<AccountId>, // Participants of current event
     created_by: AccountId,
     created_at: u64,
@@ -60,18 +54,16 @@ pub struct EventStats {
     total_actions: u64,
 }
 
-#[derive(Clone)]
-#[derive(Debug, BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+#[derive(Clone, Debug, BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct ActionData {
     timestamp: u64,
     username: String,
     qr_string: String,
-    reward_index: usize,    
+    reward_index: usize,
 }
 
-#[derive(Clone)]
-#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
+#[derive(Clone, BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct ActionResult {
     index: usize,
@@ -81,8 +73,7 @@ pub struct ActionResult {
 }
 
 /// This is format of output via JSON for the user balance.
-#[derive(Clone)]
-#[derive(BorshSerialize, BorshDeserialize, Serialize, Deserialize, Debug)]
+#[derive(Clone, BorshSerialize, BorshDeserialize, Serialize, Deserialize, Debug)]
 #[serde(crate = "near_sdk::serde")]
 pub struct UserBalance {
     pub karma_balance: u64,
@@ -95,21 +86,20 @@ pub struct UserBalance {
 pub struct Event {
     pub nonce: u32, // Id
     pub data: EventData,
-    pub stats: EventStats,    
+    pub stats: EventStats,
 }
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
-    authors: UnorderedSet<AccountId>, // Approved list of authors
-    public_events: UnorderedSet<u32>, // List of public events (premoderated)
+    public_events: UnorderedSet<u32>, // List of ongoing events
     ongoing_events: LookupMap<AccountId, HashSet<u32>>, // List of ongoing events by owner account
     events: UnorderedMap<u32, Event>, // All events data and stats
 
     // NFT implementation
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
-    
+
     // Event statistics and history (event_id -> actions)
     actions: LookupMap<u32, Vector<ActionData>>, // History of all user actions
     // Balance sheet for each user (event_id -> account_id -> balance)
@@ -122,7 +112,6 @@ enum StorageKey {
     BalancesRoot,
     Actions { event_id: u32 },
     Balances { event_id: u32 },
-    Authors,
     PublicEvents,
     Events,
     Ongoing,
@@ -131,19 +120,18 @@ enum StorageKey {
     TokenMetadata,
     Enumeration,
     Approval,
-        
 }
 
 // Contract NFT metadata
-use constants::DATA_IMAGE_SVG_NEAR_ICON;
 use constants::BASE_URI;
+use constants::DATA_IMAGE_SVG_NEAR_ICON;
 
 #[near_bindgen]
 impl Contract {
     /// Initializes the contract owned by `owner_id` with
     /// default metadata (for example purposes only).    
     #[init]
-    pub fn new() -> Self {        
+    pub fn new() -> Self {
         assert!(!env::state_exists(), "Already initialized");
 
         // Common metadata for all minted SBT
@@ -156,12 +144,11 @@ impl Contract {
             reference: None,
             reference_hash: None,
         };
-        metadata.assert_valid();        
+        metadata.assert_valid();
 
         // Init
         Self {
-            authors: UnorderedSet::new(StorageKey::Authors), // Approved list of authors
-            public_events: UnorderedSet::new(StorageKey::PublicEvents), // List of public events (premoderated)                        
+            public_events: UnorderedSet::new(StorageKey::PublicEvents), // List of public events (premoderated)
             ongoing_events: LookupMap::new(StorageKey::Ongoing),
             events: UnorderedMap::new(StorageKey::Events),
             actions: LookupMap::new(StorageKey::ActionsRoot),
@@ -173,18 +160,23 @@ impl Contract {
                 Some(StorageKey::Enumeration),
                 Some(StorageKey::Approval),
             ),
-            metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),            
+            metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
         }
     }
-    
-    /// Initiate next event 
+
+    /// Initiate next event
     #[payable]
-    pub fn start_event(&mut self, event_data: EventData) -> u32 {        
+    pub fn start_event(&mut self, event_data: EventData) -> u32 {
         let user_id = env::predecessor_account_id();
         let timestamp: u64 = env::block_timestamp();
         let hash: Vec<u8> = env::sha256(&event_data.try_to_vec().unwrap());
         let nonce: u32 = read_be_u32(&mut hash.as_slice());
-        assert!( self.events.get(&nonce).is_none( )); // assert event doesnt exist
+
+        // assert event doesnt exist
+        assert!(
+            self.events.get(&nonce).is_none(),
+            "Event with provided id doesn't exist"
+        );
 
         let initial_stats = EventStats {
             participants: HashSet::new(),
@@ -197,14 +189,20 @@ impl Contract {
         };
 
         // Empty history of event actions and user balances //https://www.near-sdk.io/contract-structure/nesting
-        self.actions.insert(&nonce, &Vector::new(StorageKey::Actions { event_id: nonce }));
-        self.balances.insert(&nonce, &LookupMap::new(StorageKey::Balances { event_id: nonce }));
+        self.actions.insert(
+            &nonce,
+            &Vector::new(StorageKey::Actions { event_id: nonce }),
+        );
+        self.balances.insert(
+            &nonce,
+            &LookupMap::new(StorageKey::Balances { event_id: nonce }),
+        );
 
         // Create new event
         let event = Event {
             nonce, // Unique event ID
             data: event_data,
-            stats: initial_stats,            
+            stats: initial_stats,
         };
 
         // Update index by user
@@ -214,33 +212,43 @@ impl Contract {
 
         // Update public events set
         self.public_events.insert(&nonce);
-        
+
         // Add event to the list
         self.events.insert(&nonce, &event);
-        log!("Successfully started by {} an event with id {}", user_id, nonce);
+        log!(
+            "Successfully started by {} an event with id {}",
+            user_id,
+            nonce
+        );
 
         nonce // return event_id = nonce
     }
 
     /// Stop and put event to archive (only for an owner of event)
     #[payable]
-    pub fn stop_event(&mut self, event_id: u32) {        
+    pub fn stop_event(&mut self, event_id: u32) {
         let user_id = env::predecessor_account_id();
         let timestamp: u64 = env::block_timestamp();
         let mut user_events = self.ongoing_events.get(&user_id).unwrap();
         // assert it is up to us to stop event
-        assert!(user_events.get(&event_id.clone()).is_some()); 
+        assert!(user_events.get(&event_id.clone()).is_some());
         // Remove from user ongoing events
         user_events.remove(&event_id);
         self.ongoing_events.insert(&user_id, &user_events);
 
         // Remove from public events set
-        self.public_events.remove(&event_id);       
+        self.public_events.remove(&event_id);
 
         // Fix actual finish time
         let mut event = self.events.get(&event_id).unwrap();
         event.stats.stopped_at = Some(timestamp);
         self.events.insert(&event_id, &event);
+
+        log!(
+            "Successfully stopped by {} an event with id {}",
+            user_id,
+            event_id
+        );
     }
 
     /// Issue reward token
@@ -253,8 +261,9 @@ impl Contract {
         let event = self.events.get(&event_id).unwrap();
         let quests = event.data.quests.clone();
         let quest = quests.get(reward_index).unwrap();
-        let rand: u8 = *env::random_seed().get(0).unwrap();                                                                     
-        let token_id_with_timestamp: String = format!("{}:{}:{}:{}", &event_id, &reward_index, &timestamp, rand);
+        let rand: u8 = *env::random_seed().get(0).unwrap();
+        let token_id_with_timestamp: String =
+            format!("{}:{}:{}:{}", &event_id, &reward_index, &timestamp, rand);
         let media_url: String = format!("{}", quest.reward_uri);
         let media_hash = Base64VecU8(env::sha256(media_url.as_bytes()));
 
@@ -268,42 +277,69 @@ impl Contract {
             expires_at: None,
             starts_at: None,
             updated_at: None,
-            extra: None,
+            extra: Some("#hashtag".to_string()),
             reference: None,
             reference_hash: None,
         };
 
-        // Mint achievement reward                                
+        // Mint achievement reward
         let root_id = AccountId::try_from(contract_id).unwrap();
 
-        self.nft_mint(token_id_with_timestamp.clone(), receiver_id.clone(), token_metadata.clone());
-        log!("Success! Minting NFT for {}! TokenID = {}", root_id.clone(), token_id_with_timestamp.clone());
+        self.nft_mint(
+            token_id_with_timestamp.clone(),
+            receiver_id.clone(),
+            token_metadata.clone(),
+        );
+        log!(
+            "Success! Minting NFT for {}! TokenID = {}",
+            root_id.clone(),
+            token_id_with_timestamp.clone()
+        );
     }
 
     #[payable]
-    pub fn checkin(&mut self, event_id: u32, username: String, request: String) -> Option<ActionResult> {
-        // Assert event is active        
+    pub fn checkin(
+        &mut self,
+        event_id: u32,
+        username: String,
+        request: String,
+    ) -> Option<ActionResult> {
+        // Assert event is active
+        assert!(
+            self.public_events.contains(&event_id),
+            "No event with this id is running"
+        );
+
+        // Check if event is not expired
         let timestamp: u64 = env::block_timestamp();
         let mut event = self.events.get(&event_id).unwrap();
-        assert!( event.data.finish_time > timestamp, "No event with this id is running" );
+        assert!(
+            event.data.finish_time > timestamp,
+            "Event with given id is expired"
+        );
 
         // Check if account seems valid
-        assert!( AccountId::try_from(username.clone()).is_ok(), "Valid account is required" );
+        assert!(
+            AccountId::try_from(username.clone()).is_ok(),
+            "Valid account is required"
+        );
         let user_account_id = AccountId::try_from(username.clone()).unwrap();
-                        
+
         // Match QR code to quest
-        let qr_string = request.clone();        
+        let qr_string = request.clone();
         let quests = event.data.quests.clone();
         let mut reward_index = 0;
         for quest in &quests {
             if let Some(request_prefix) = request.get(0..quest.qr_prefix_len) {
                 let hashed_input = env::sha256(request_prefix.as_bytes());
                 let hashed_input_hex = hex::encode(&hashed_input);
-                if hashed_input_hex == quest.qr_prefix_enc { break };
-            }         
-            reward_index = reward_index + 1;            
+                if hashed_input_hex == quest.qr_prefix_enc {
+                    break;
+                };
+            }
+            reward_index = reward_index + 1;
         }
-        
+
         let action_data = ActionData {
             username: username.clone(),
             qr_string: qr_string.clone(),
@@ -313,54 +349,71 @@ impl Contract {
 
         log!("Action data: {:?}", action_data);
 
-        // Register checkin data        
+        // Register checkin data
         let mut stats = event.stats.clone();
 
         // Check if we have a new user
         if stats.participants.insert(user_account_id.clone()) {
             stats.total_users += 1;
-            
+
             // Initial balance
-            self.balances.get(&event_id).unwrap().insert(&user_account_id, &UserBalance {
-                karma_balance: 0,
-                quests_status: vec![false; quests.len()],
-            });
+            self.balances.get(&event_id).unwrap().insert(
+                &user_account_id,
+                &UserBalance {
+                    karma_balance: 0,
+                    quests_status: vec![false; quests.len()],
+                },
+            );
         }
 
-        // Register action 
+        // Register action
         let mut actions = self.actions.get(&event_id).unwrap();
         actions.push(&action_data);
         stats.total_actions += 1;
 
         // Update contract state
-        self.actions.insert(&event_id, &actions);                
+        self.actions.insert(&event_id, &actions);
 
         // Check if we've been awarded a reward
-        if let Some(quest) = quests.get(reward_index) {  
-            // Update state if we are lucky          
+        if let Some(quest) = quests.get(reward_index) {
+            // Update state if we are lucky
             stats.total_rewards += 1;
             event.stats = stats;
 
             // Update user balance
-            let mut balance = self.balances.get(&event_id).unwrap().get(&user_account_id).expect("ERR_NOT_REGISTERED");
+            let mut balance = self
+                .balances
+                .get(&event_id)
+                .unwrap()
+                .get(&user_account_id)
+                .expect("ERR_NOT_REGISTERED");
             balance.karma_balance += 1; // Number of successfull actions
 
-            // Do we have this reward already            
-            if balance.quests_status[reward_index] { // Yes (no reward then)
+            // Do we have this reward already
+            if balance.quests_status[reward_index] {
+                // Yes (no reward then)
                 self.events.insert(&event_id, &event);
-                
+
                 return Some(ActionResult {
                     index: reward_index,
                     got: true,
                     title: quest.reward_title.clone(),
                     description: quest.reward_description.clone(),
-                });                
-            } else { // No
+                });
+            } else {
+                // No
                 balance.quests_status[reward_index] = true;
-                self.balances.get(&event_id).unwrap().insert(&user_account_id, &balance);
+                self.balances
+                    .get(&event_id)
+                    .unwrap()
+                    .insert(&user_account_id, &balance);
 
                 // NFT Part (issue token)
-                self.issue_nft_reward(user_account_id.clone(), event_id.clone(), reward_index.clone());
+                self.issue_nft_reward(
+                    user_account_id.clone(),
+                    event_id.clone(),
+                    reward_index.clone(),
+                );
 
                 self.events.insert(&event_id, &event);
                 return Some(ActionResult {
@@ -369,10 +422,10 @@ impl Contract {
                     title: quest.reward_title.clone(),
                     description: quest.reward_description.clone(),
                 });
-            }                                     
+            }
         } else {
             // Update stats
-            event.stats = stats;   
+            event.stats = stats;
             log!("No reward for this checkin! User: {}", username);
             self.events.insert(&event_id, &event);
             None
