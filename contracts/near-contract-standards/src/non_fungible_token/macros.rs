5d63b9812fe081e1f9b912c28f46bfcff0ca3c04
SergantChe <sergei.alex.kozlov@gmail.com>
2023-04-21 13:57:27 +0300

    update near-contract-standards

--- contracts/near-contract-standards/src/non_fungible_token/macros.rs
+++ contracts/near-contract-standards/src/non_fungible_token/macros.rs
@@ -3,14 +3,57 @@
 #[macro_export]
 macro_rules! impl_non_fungible_token_core {
     ($contract: ident, $token: ident) => {
-        use $crate::non_fungible_token::core::NonFungibleTokenCore;        
+        use $crate::non_fungible_token::core::NonFungibleTokenCore;
+        use $crate::non_fungible_token::core::NonFungibleTokenResolver;
 
         #[near_bindgen]
-        impl NonFungibleTokenCore for $contract {        
+        impl NonFungibleTokenCore for $contract {
+            #[payable]
+            fn nft_transfer(
+                &mut self,
+                receiver_id: AccountId,
+                token_id: TokenId,
+                approval_id: Option<u64>,
+                memo: Option<String>,
+            ) {
+                self.$token.nft_transfer(receiver_id, token_id, approval_id, memo)
+            }
+
+            #[payable]
+            fn nft_transfer_call(
+                &mut self,
+                receiver_id: AccountId,
+                token_id: TokenId,
+                approval_id: Option<u64>,
+                memo: Option<String>,
+                msg: String,
+            ) -> PromiseOrValue<bool> {
+                self.$token.nft_transfer_call(receiver_id, token_id, approval_id, memo, msg)
+            }
+
             fn nft_token(&self, token_id: TokenId) -> Option<Token> {
                 self.$token.nft_token(token_id)
             }
-        }        
+        }
+
+        #[near_bindgen]
+        impl NonFungibleTokenResolver for $contract {
+            #[private]
+            fn nft_resolve_transfer(
+                &mut self,
+                previous_owner_id: AccountId,
+                receiver_id: AccountId,
+                token_id: TokenId,
+                approved_account_ids: Option<std::collections::HashMap<AccountId, u64>>,
+            ) -> bool {
+                self.$token.nft_resolve_transfer(
+                    previous_owner_id,
+                    receiver_id,
+                    token_id,
+                    approved_account_ids,
+                )
+            }
+        }
     };
 }
 
